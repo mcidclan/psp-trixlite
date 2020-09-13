3469f9c53a34aabb354b6a12c9f067aa4f7a04c5
m-c/d <534348+mcidclan@users.noreply.github.com>
2020-09-13 11:11:28 +0200

    -Reviews the code to get blocks in color.

--- main.c
+++ main.c
@@ -7,6 +7,10 @@
 #define NBR_PIECES 4
 #define BLOCK_SIZE 10
 
+PSP_MODULE_INFO("tl", 0, 1, 1);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
+PSP_HEAP_SIZE_KB(-1024);
+
 void sceDmacMemcpy(void *dst, const void *src, int size);
 int atexit(void(*function)(void)) {
     function();
@@ -15,45 +19,45 @@ int atexit(void(*function)(void)) {
 void exit(int status) {
     while(1);
 }
-
-PSP_MODULE_INFO("tl", 0, 1, 1);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
-PSP_HEAP_SIZE_KB(-1024);
+typedef struct {
+    u64 data;
+    u32 color;
+} Piece;
 
 static u16 __attribute__((aligned(16))) rows[NBR_ROWS] = {0};
 
-static u64 __attribute__((aligned(16))) pieces[NBR_PIECES] = {
-    0b0000000010000000000000001000000000000000100000000000000010000000,
-    0b0000000010000000000000111000000000000000000000000000000000000000,
-    0b0000000010000000000000011100000000000000000000000000000000000000,
-    0b0000000011000000000000011000000000000000000000000000000000000000
+static Piece __attribute__((aligned(16))) pieces[NBR_PIECES] = {
+    {0x80008000800080, 0xFFFF0000},
+    {0x80038000000000, 0xFF00FF00},
+    {0x8001C000000000, 0xFF0000FF},
+    {0xC0018000000000, 0xFF00FFFF}
 };
 
-static u32* const vram[3] = {
+static u32* const vram[2] = {
     ((u32*)0x44000000),
     ((u32*)0x44088000)
 };
 
-void drawBlock(const u8 x, const u8 y) {
+void drawBlock(const u8 vid, const u8 x, const u8 y, const u32 color) {
     u8 i = BLOCK_SIZE;
     while(--i) {
         u8 j = BLOCK_SIZE;
         while(--j) {
-            vram[0][(i+x*BLOCK_SIZE) | ((j+y*BLOCK_SIZE) << 9)] = 0xFFFFFFFF;
+            vram[vid][(i+x*BLOCK_SIZE) | ((j+y*BLOCK_SIZE) << 9)] = color;
         }
     }
 }
 
-void drawRow(const u16 value, const u8 y) {
+void drawRow(const u8 vid, const u16 value, const u8 y, const u32 color) {
     u8 x = 16;
     while(x--) {
         if(value & (0x8000 >> x)) {
-            drawBlock(x, y);
+            drawBlock(vid, x, y, color);
         }
     }
 }
 
-void draw4Rows(const u64 value, u8 y) {
+void draw4Rows(const u8 vid, const u64 value, u8 y, const u32 color) {
     u8 x = 64;
     while(x--) {
         const u8 _x = (x % 16);
@@ -61,7 +65,7 @@ void draw4Rows(const u64 value, u8 y) {
             y++;
         }
         if(value & (0x8000000000000000 >> x)) {
-            drawBlock(_x, y);
+            drawBlock(vid, _x, y, color);
         }
     }
 }
@@ -76,13 +80,21 @@ int main() {
     sceDisplaySetFrameBuf(vram[0], 512,
     PSP_DISPLAY_PIXEL_FORMAT_8888, PSP_DISPLAY_SETBUF_IMMEDIATE);
 
-    u8 step = 0;
+    u8 step = 0, id = 0;
     u64 piece = 0, keytick = 0, runtick = 0;
 
     sceRtcGetCurrentTick(&runtick);
     keytick = runtick;
 
     const u64 tickres = sceRtcGetTickResolution() / 1000;    
+
+    u8 i = NBR_ROWS;
+    while(i--) {
+        if(i == NBR_ROWS - 1) {
+            rows[i] = 0xFFFF;
+        } else rows[i] = 0x8001;
+        drawRow(1, rows[i], i, 0xFF808080);
+    }
 
     do {
         sceDmacMemcpy(vram[0], vram[1], 512*272*4);
@@ -92,11 +104,12 @@ int main() {
         sceRtcGetCurrentTick(&tick);
 
         if(!piece) {
-            piece = pieces[tick  % 4];
+            id = tick % 4;
+            piece = pieces[id].data;
             step = 0;
         }
 
-        draw4Rows(piece, step);
+        draw4Rows(0, piece, step, pieces[id].color);
 
         if((tick - runtick) / tickres >= 250) {
             step++;
@@ -105,23 +118,23 @@ int main() {
 
         u16* const target = &rows[step];
 
-        if((tick - keytick) / tickres >= 125) {
+        if((tick - keytick) / tickres >= 250) {
+            const u64 prev = piece;
             if(pad.Buttons & PSP_CTRL_LEFT) {
                 piece <<= 1;
             } else if(pad.Buttons & PSP_CTRL_RIGHT) {
                 piece >>= 1;
             }
+            if(isCollid(target, piece)) {
+                piece = prev;
+            }
             keytick = tick;
         }
 
-        if(step >= NBR_ROWS - 4 || isCollid(target, piece)) {
+        if(isCollid(target, piece)) {
             *(u64*)(target-1) |= piece;
+            draw4Rows(1, piece, step-1, pieces[id].color);
             piece = 0;
-        }
-
-        u8 i = NBR_ROWS;
-        while(i--) {
-            drawRow(rows[i], i);
         }
 
         sceDisplayWaitVblankStart();
